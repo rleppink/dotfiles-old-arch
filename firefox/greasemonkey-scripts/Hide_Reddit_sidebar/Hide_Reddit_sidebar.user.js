4e301f005cb0a6841ebae08fc83ec3fbad10bcde
Ronald Leppink <ronaldleppink@gmail.com>
2016-03-15 13:49:36 +0100

    Updated reddit script to middle column mode

--- firefox/greasemonkey-scripts/Hide_Reddit_sidebar/Hide_Reddit_sidebar.user.js
+++ firefox/greasemonkey-scripts/Hide_Reddit_sidebar/Hide_Reddit_sidebar.user.js
@@ -6,17 +6,33 @@
 // @grant       none
 // ==/UserScript==
 
-$('.side').hide();
-$('#header-bottom-right .flat-list.hover').append('<span class="separator">|</span><a id="toggleSidebar">toggle sidebar</a>');
-$('#toggleSidebar').click(function() {
-  $('.side').toggle();
-});
-$('#toggleSidebar').css('cursor', 'pointer');
 
+function hideSideBar() {
+  $('.side').hide();
+  $('#header-bottom-right .flat-list.hover').append('<span class="separator">|</span><a id="toggleSidebar">toggle sidebar</a>');
+  $('#toggleSidebar').click(function() {
+    $('.side').toggle();
+  });
+  $('#toggleSidebar').css('cursor', 'pointer');
+};
 
-$('form.usertext.cloneable').hide();
-$('.menuarea').append('<a id="toggleComment">toggle comment box</a>');
-$('#toggleComment').click(function() {
-  $('form.usertext.cloneable').toggle();
-});
-$('#toggleComment').css('cursor', 'pointer');
+function hideCommentBox() {
+  $('form.usertext.cloneable').hide();
+  $('.menuarea').append('<a id="toggleComment">toggle comment box</a>');
+  $('#toggleComment').click(function() {
+    $('form.usertext.cloneable').toggle();
+  });
+  $('#toggleComment').css('cursor', 'pointer');  
+};
+
+function middleColumnComments() {
+  $('div.content')[0].style.width = "1000px";
+  $('div.content')[0].style.margin = "0 auto";
+  $('div.content')[0].style.marginTop = "1.5em";
+}
+
+window.onload = function () {
+  hideSideBar();
+  hideCommentBox();
+  middleColumnComments();
+};
