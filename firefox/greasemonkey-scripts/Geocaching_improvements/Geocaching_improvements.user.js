639427c082c904d3e1e936f05ba3ca2d91006e12
Ronald Leppink <ronaldleppink@gmail.com>
2016-11-22 15:47:14 +0100

    Cleaned up unused files

--- firefox/greasemonkey-scripts/Geocaching_improvements/Geocaching_improvements.user.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name        geocaching.com: Geocaching improvements
-// @namespace   com.rleppink.geocachingimprovements
-// @version     1
-// @grant       none
-// ==/UserScript==
-
-$("#searchResultsTable").find("[data-premium='']").hide();
-$("#searchResultsTable").find(".disabled").hide();
